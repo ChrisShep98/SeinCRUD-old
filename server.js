d8bb8409717891c467f66a5ab4e36a9ba075e032
ChrisShep98 <shepardchristopher1@gmail.com>
2022-06-21 19:32:34 -0400

    deleted PORT variable in server and added to env

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const PORT = 1989
 const cors = require('cors')
 require('dotenv').config()
 
@@ -85,6 +84,6 @@ app.delete('/deleteEpisode', (request, response) => {
 
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
+app.listen(process.env.PORT || 8080, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
